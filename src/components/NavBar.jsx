519fe13bd3f3fc5ac8f28a25fcb70b1a2be994fd
matheus <matheusaltrao2@gmail.com>
2023-04-09 22:08:18 -0400

    add hover buttons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,30 @@
 import { ClosedCaptioning, List, X } from 'phosphor-react'
 import React, { useState } from 'react'
-import logo from '../assets/fiveiconsemfundo.png'
+import logo from '../assets/logoCyan.svg'
 
 const NavBar = () => {
 
   const [menu, setMenu] = useState(false)
-
   function showMenu() {
     setMenu(!menu)
   }
 
   return (
-    <nav className='w-full fixed top-2 px-4  left-0 right-0 z-10  max-w-[1200px] mx-auto'>
+    <nav className='w-full fixed top-2 md:px-4 px-0  left-0 right-0 z-10  max-w-[1200px] mx-auto'>
 
 
-      <div className='w-full  px-4 py-2 bg-red-300 h-full  flex items-center justify-between rounded-xl glass  ' >
+      <div className='w-full  px-4 py-4  h-full  flex items-center justify-between md:rounded-xl  glass  ' >
 
 
-
-        <div className=' w-10 md:w-15 object-cover'>
+        <div className=' w-12 md:w-15 object-cover'>
           <img className='animate-top4 ' src={logo} alt="" />
         </div>
 
-        <List onClick={showMenu} className='flex md:hidden icon text-white cursor-pointer' />
+        <List onClick={showMenu} className='flex  md:hidden text-[2rem] text-white cursor-pointer' />
 
         <ul className={`md:flex items-center  menu ${menu ? 'active' : ''} animate-top6   `} >
           <div className='absolute top-0 left-0'>
-            <X onClick={showMenu} className='icon relative top-4 left-8 text-sky-300 md:hidden' />
+            <X onClick={showMenu} className='text-[2rem] cursor-pointer relative top-4 left-8 text-sky-300 md:hidden' />
           </div>
 
           <li onClick={showMenu} className='nav text-slate-300' ><a href="#home">Início</a></li>
